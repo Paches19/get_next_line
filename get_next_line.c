0e344e9eea80dd077b1c6a8584c504976738987a
root <root@LAPTOP-UTKVD2O1.localdomain>
2022-10-24 21:01:14 +0200

    ayuda de la platas

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: adpachec <adpachec@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 10:42:50 by adpachec          #+#    #+#             */
-/*   Updated: 2022/10/24 15:57:21 by adpachec         ###   ########.fr       */
+/*   Updated: 2022/10/24 19:49:30 by adpachec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,12 +99,12 @@ char	*write_last(char *str, int len_s1, const char *s2, int i)
 	return (NULL);
 }
 
-char	*ft_strjoin(char *s1, char const *s2)
+char	*ft_strjoin(char *s1, char const **s2)
 {
 	char			*str;
 	int				i;
 	const size_t	len_s1 = ft_strlen(s1);
-	const size_t	len_s2 = ft_strlen_gnl(s2);
+	const size_t	len_s2 = ft_strlen_gnl(*s2);
 
 	str = (char *) ft_calloc(sizeof(char) * (len_s1 + len_s2 + 2), 1);
 	if (!str)
@@ -120,20 +120,19 @@ char	*ft_strjoin(char *s1, char const *s2)
 			str[i] = s1[i];
 	}
 	i = -1;
-	while (s2[++i] && s2[i] != '\n')
-		str[len_s1 + i] = s2[i];
-	write_last(str, len_s1, s2, i);
-	free(s1);
+	while (*s2[++i] && *s2[i] != '\n')
+		str[len_s1 + i] = *s2[i];
+	write_last(str, len_s1, *s2, i);
 	return (str);
 }
 
-char	*copy_buf(char *save_read, char *buf)
+char	*copy_buf(char **save_read, char *buf)
 {
 	int		i;
 	int		j;
 	char	*temp;
 
-	temp = (char *) ft_calloc(ft_strlen(save_read) + ft_strlen(buf) + 1, 1);
+	temp = (char *) ft_calloc(ft_strlen(*save_read) + ft_strlen(buf) + 1, 1);
 	i = 0;
 	while (temp[i])
 		++i;
@@ -141,53 +140,50 @@ char	*copy_buf(char *save_read, char *buf)
 	while (buf[++j])
 		temp[i++] = buf[j];
 	temp[i] = '\0';
-	free (save_read);
+	free (*save_read);
 	return (temp);
 }
 
-char	*delete_line(char *save_read)
+char	*delete_line(char **save_read)
 {
 	char	*new_read;
 	int		len;
 	int		i;
 	int		j;
 
-	printf("\nsr delete: %s", save_read);
+	printf("\nsr delete: %s", *save_read);
 	i = 0;
-	if (!save_read[0])
-		return (NULL);
-	while (save_read[i] && save_read[i] != '\n')
+	while (*save_read[i] && *save_read[i] != '\n')
 		++i;
-	len = ft_strlen(save_read) - i;
+	len = ft_strlen(*save_read) - i;
 	new_read = (char *) ft_calloc(1, len + 1);
 	j = 0;
-	while (save_read[++i])
-		new_read[j++] = save_read[i];
-	new_read[++j] = '\0';
+	while (*save_read[++i])
+		new_read[j++] = *save_read[i];
 	printf("\ndelete: %s", new_read);
-	free(save_read);
+	free(*save_read);
 	return (new_read);
 }
 
-char	*save_to_line(int fd, char *line, char *save_read, ssize_t *read_len)
+char	*save_to_line(int fd, char *line, char **save_read, ssize_t *read_len)
 {
 	char	*buf;
 
-	if (!save_read)
+	if (!ft_strlen(*save_read))
 	{
 		buf = (char *) ft_calloc(sizeof(char) * (BUFFER_SIZE + 2), 1);
 		*read_len = read(fd, buf, BUFFER_SIZE);
-		save_read = copy_buf(save_read, buf);
+		*save_read = copy_buf(&(*save_read), buf);
+		free(buf);
 	}
-	if (!save_read)
+	if (!*save_read)
 		return (NULL);
-	printf("\nsr stl: %s", save_read);
+	printf("\nsr stl: %s", *save_read);
 	if (*read_len > 0)
-		line = ft_strjoin(line, save_read);
+		line = ft_strjoin(line, &save_read);
 	printf("\nline: %s", line);
 	if (!line)
 		return (NULL);
-	free(buf);
 	return (line);
 }
 
@@ -197,7 +193,7 @@ char	*read_fd(int fd, char *line)
 	static char	*save_read;
 
 	read_len = 0;
-	line = save_to_line(fd, line, save_read, &read_len);
+	line = save_to_line(fd, line, &save_read, &read_len);
 	printf("\nline rfd: %s", line);
 	printf("\nsr rfd: %s", save_read);
 	save_read = delete_line(save_read);
@@ -216,7 +212,7 @@ char	*read_fd(int fd, char *line)
 
 char	*get_next_line(int fd)
 {
-	char		*line;
+	char	*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
