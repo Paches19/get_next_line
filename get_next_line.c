1dbc1d623059e763696b57dc64befbb9d2a8afc0
Paches19 <pachees19@gmail.com>
2022-10-25 18:07:17 +0200

    casi bien

--- get_next_line.c
+++ get_next_line.c
@@ -6,31 +6,18 @@
 /*   By: adpachec <adpachec@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 10:42:50 by adpachec          #+#    #+#             */
-/*   Updated: 2022/10/24 19:49:30 by adpachec         ###   ########.fr       */
+/*   Updated: 2022/10/25 13:20:46 by adpachec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-static void	ft_bzero(void *s, size_t n)
-{
-	unsigned char	*ptr;
-	size_t			i;
-
-	ptr = (unsigned char *) s;
-	i = 0;
-	while (i < n && n > 0)
-	{
-		ptr[i] = 0;
-		++i;
-	}
-}
-
 static char	*ft_calloc(size_t count, size_t size)
 {
 	char			*ptr;
 	const size_t	len = count * size;
+	size_t			i;
 
 	if (!count)
 		return (malloc(0));
@@ -39,35 +26,36 @@ static char	*ft_calloc(size_t count, size_t size)
 	ptr = (char *) malloc(len);
 	if (!ptr)
 		return (NULL);
-	ft_bzero(ptr, len);
+	i = 0;
+	while (i < len && len > 0)
+	{
+		ptr[i] = 0;
+		++i;
+	}
 	return (ptr);
 }
 
-static size_t	ft_strlen_gnl(const char *s)
+size_t	ft_strlen(char *s, int gnl)
 {
 	size_t	len;
 
+	len = 0;
 	if (!s)
 		return (0);
-	len = 0;
-	while (s[len] && s[len] != '\n')
-		++len;
+	if (!gnl)
+	{
+		while (s[len])
+			++len;
+	}
+	else
+	{
+		while (s[len] && s[len] != '\n')
+			++len;
+	}
 	return (len);
 }
 
-size_t	ft_strlen(const char *s)
-{
-	size_t	len;
-
-	if (!s)
-		return (0);
-	len = 0;
-	while (s[len])
-		++len;
-	return (len);
-}
-
-char	*ft_strchr(const char *s, int c)
+char	*ft_strchr(char *s, int c)
 {
 	int		i;
 
@@ -83,29 +71,32 @@ char	*ft_strchr(const char *s, int c)
 	return (NULL);
 }
 
-char	*write_last(char *str, int len_s1, const char *s2, int i)
+char	*write_last(char *str, int len_s1, char s2, int i)
 {
-	if (s2[len_s1 + i] == '\n')
+	//printf("\ns2i: %c\n", s2);
+	if (s2 == '\n')
 	{
-		str[len_s1 + i] = s2[i];
-		str[len_s1 + i + 1] = '\0';
+		str[len_s1 + i] = s2;
+		//printf("\nhola\n");
 		return (str);
 	}
-	else
-	{
-		str[len_s1 + i] = '\0';
-		return (str);
-	}
-	return (NULL);
+	return (str);
 }
 
-char	*ft_strjoin(char *s1, char const **s2)
+char	*ft_strjoin(char *s1, char **s2)
 {
 	char			*str;
 	int				i;
-	const size_t	len_s1 = ft_strlen(s1);
-	const size_t	len_s2 = ft_strlen_gnl(*s2);
+	const size_t	len_s1 = ft_strlen(s1, 0);
+	const size_t	len_s2 = ft_strlen(*s2, 1);
 
+	//printf("\nlen1: %zu", len_s1);
+	//printf("\nlen2: %zu", len_s2);
+	if (!(*s2)[0])
+	{
+		free (*s2);
+		return (s1);
+	}
 	str = (char *) ft_calloc(sizeof(char) * (len_s1 + len_s2 + 2), 1);
 	if (!str)
 	{
@@ -120,9 +111,11 @@ char	*ft_strjoin(char *s1, char const **s2)
 			str[i] = s1[i];
 	}
 	i = -1;
-	while (*s2[++i] && *s2[i] != '\n')
-		str[len_s1 + i] = *s2[i];
-	write_last(str, len_s1, *s2, i);
+	//printf("\ns2: %d", str[len_s1 + 0]);
+	while ((*s2)[++i] && (*s2)[i] != '\n')
+		str[len_s1 + i] = (*s2)[i];
+	write_last(str, len_s1, (*s2)[i], i);
+	free (s1);
 	return (str);
 }
 
@@ -132,14 +125,14 @@ char	*copy_buf(char **save_read, char *buf)
 	int		j;
 	char	*temp;
 
-	temp = (char *) ft_calloc(ft_strlen(*save_read) + ft_strlen(buf) + 1, 1);
+	temp = (char *) ft_calloc(ft_strlen(*save_read, 0) + ft_strlen(buf, 0) \
+	+ 1, 1);
 	i = 0;
 	while (temp[i])
 		++i;
 	j = -1;
 	while (buf[++j])
 		temp[i++] = buf[j];
-	temp[i] = '\0';
 	free (*save_read);
 	return (temp);
 }
@@ -151,17 +144,30 @@ char	*delete_line(char **save_read)
 	int		i;
 	int		j;
 
-	printf("\nsr delete: %s", *save_read);
+	//printf("\nsr delete: %s", *save_read);
+	if ((*save_read)[0] == '\0')
+	{
+		//printf("\ndevuevlo null");
+		free (*save_read);
+		return (NULL);
+	}
 	i = 0;
-	while (*save_read[i] && *save_read[i] != '\n')
+	while ((*save_read)[i] && (*save_read)[i] != '\n')
 		++i;
-	len = ft_strlen(*save_read) - i;
+	len = ft_strlen(*save_read, 0) - i;
+	//printf("\ni: %d", i);
+	if ((*save_read)[i] == '\0')
+	{
+		//printf("\ndevuevlo null");
+		free (*save_read);
+		return (NULL);
+	}
 	new_read = (char *) ft_calloc(1, len + 1);
 	j = 0;
-	while (*save_read[++i])
-		new_read[j++] = *save_read[i];
-	printf("\ndelete: %s", new_read);
-	free(*save_read);
+	while ((*save_read)[++i])
+		new_read[j++] = (*save_read)[i];
+	//printf("\ndelete: %s", new_read);
+	free (*save_read);
 	return (new_read);
 }
 
@@ -169,21 +175,21 @@ char	*save_to_line(int fd, char *line, char **save_read, ssize_t *read_len)
 {
 	char	*buf;
 
-	if (!ft_strlen(*save_read))
+	if (!(*save_read) || (*save_read)[0] == '\0')
 	{
-		buf = (char *) ft_calloc(sizeof(char) * (BUFFER_SIZE + 2), 1);
+		//printf("\nnew save read\n");
+		buf = (char *) ft_calloc(sizeof(char) * (BUFFER_SIZE + 1), 1);
 		*read_len = read(fd, buf, BUFFER_SIZE);
 		*save_read = copy_buf(&(*save_read), buf);
+		//printf("\nsr new: %s", *save_read);
 		free(buf);
 	}
 	if (!*save_read)
 		return (NULL);
-	printf("\nsr stl: %s", *save_read);
-	if (*read_len > 0)
-		line = ft_strjoin(line, &save_read);
-	printf("\nline: %s", line);
-	if (!line)
-		return (NULL);
+	//printf("\nsr stl: %d", (*save_read)[0]);
+	if (*read_len > 0 || (*save_read)[0] != '\0')
+		line = ft_strjoin(line, &(*save_read));
+	//printf("\nline: %s", line);
 	return (line);
 }
 
@@ -194,18 +200,17 @@ char	*read_fd(int fd, char *line)
 
 	read_len = 0;
 	line = save_to_line(fd, line, &save_read, &read_len);
-	printf("\nline rfd: %s", line);
-	printf("\nsr rfd: %s", save_read);
-	save_read = delete_line(save_read);
-	printf("\nsr rfd: %s", save_read);
+	//printf("\nline rfd: %s", line);
+	//printf("\nsr rfd1: %s", save_read);
+	save_read = delete_line(&save_read);
+	//printf("\nsr rfd2: %s", save_read);
 	if (read_len <= 0)
 		return (NULL);
-	while (read_len > 0 && !ft_strchr(save_read, '\n') && \
-	*save_read != '\0')
+	while (read_len > 0 && !ft_strchr(line, '\n') && !ft_strchr(line, '\0'))
 	{
-		line = save_to_line(fd, line, save_read, &read_len);
-		save_read = delete_line(line);
-		printf("\nsrlast: %s", save_read);
+		line = save_to_line(fd, line, &save_read, &read_len);
+		save_read = delete_line(&save_read);
+		//printf("\nsrlast: %s", save_read);
 	}
 	return (line);
 }
@@ -218,15 +223,20 @@ char	*get_next_line(int fd)
 		return (NULL);
 	line = NULL;
 	line = read_fd(fd, line);
-	printf("\nline last: %s", line);
+	//printf("\nline last: %s", line);
 	return (line);
 }
 
-int	main()
-{
-	int fd;
+// int	main()
+// {
+// 	int fd;
 
-	fd = open("fd.txt", O_RDONLY);
-	get_next_line(fd);
-	get_next_line(fd);
-}
+// 	fd = open("fd.txt", O_RDONLY);
+// 	get_next_line(fd);
+// 	printf("fin\n\n\n");
+// 	get_next_line(fd);
+// 	printf("fin\n\n\n");
+// 	get_next_line(fd);
+// 	printf("fin\n\n\n");
+// 	get_next_line(fd);
+// }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: adpachec <adpachec@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 13:10:24 by adpachec          #+#    #+#             */
-/*   Updated: 2022/10/24 12:15:07 by adpachec         ###   ########.fr       */
+/*   Updated: 2022/10/25 12:14:08 by adpachec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,9 @@
 # endif
 
 char	*get_next_line(int fd);
-char	*ft_strjoin(char *s1, char const *s2);
+char	*ft_strjoin(char *s1, char **s2);
 void	utils(void);
-size_t	ft_strlen(const char *s);
-char	*write_last(char *str, int len_s1, const char *s2, int i);
+size_t	ft_strlen(char *s, int gnl);
+char	*write_last(char *str, int len_s1, char s2, int i);
 
 #endif
